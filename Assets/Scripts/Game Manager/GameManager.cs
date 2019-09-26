7ff45187c4536ec66180c7d64d1ee23dc87aa78f
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-09-25 20:27:55 -0700

    started working on the game manager script

---
+++ Assets/Scripts/Game Manager/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private Button shootButton;
+
+    [SerializeField]
+    private GameObject needle;
+    private GameObject[] needles;
+    private float needleDistance = 1.5f;
+    private int needleIndex;
+    [SerializeField]
+    private int howManyNeedles;
+
+    private void Awake() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
