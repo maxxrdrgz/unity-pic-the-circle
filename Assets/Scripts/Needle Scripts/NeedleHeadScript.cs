f50650f364c12d2d7610ff9acfbf54a243053fb1
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-09-29 21:29:16 -0700

    added needlehead script

---
+++ Assets/Scripts/Needle Scripts/NeedleHeadScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedleHeadScript : MonoBehaviour
+{
+    /** 
+        This function will detect collision between two needle head gameobjects.
+        If true, the game ends.
+
+        @params {Collider2D} The other Collider2D involved in this collision.
+     */
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Needle Head"){
+            print("Game Over");
+            Time.timeScale = 0f;
+        }
+    }
+}
